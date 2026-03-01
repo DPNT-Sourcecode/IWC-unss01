e12599085c5a16a7bb22132f3012fa6b1ccb8ad4
sbx_user1051 <sbx_user1051@169.254.3.29>
2026-03-01 20:54:31 +0000

    Sun Mar 01 20:54:31 UTC 2026

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -3,4 +3,6 @@ from solutions.SUM.sum_solution import SumSolution
 
 class TestSum():
     def test_sum(self):
+        assert SumSolution().compute(0, 0) == 0
         assert SumSolution().compute(1, 2) == 3
+
